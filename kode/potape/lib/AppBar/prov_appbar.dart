125b3f030b346e99a36ab8e1e98b8e35f1586266
Edbert Fortino <edbertfuu@gmail.com>
2023-05-21 19:42:44 +0700

    Penambahan template btmnav ke prov_appbar, dibuat sebagian cara pengambilan data page dari mainpage ke pro_appbar, dibuat bentuk awal dari penangpung data pages di prov_appbar

--- kode/potape/lib/AppBar/prov_appbar.dart
+++ kode/potape/lib/AppBar/prov_appbar.dart
@@ -4,8 +4,46 @@ import 'package:provider/provider.dart';
 /// UNTUK HAL APPBAR KERJAKAN DISINI
 
 class Apbr extends ChangeNotifier {
+  /// <- Data
+
+  Map<String, String> curent_page = {
+    "appbar": "search_pp",
+    "drawer": "base_drawer",
+    "body": "shop",
+    "btmnav": "base_btmnav",
+    "btm_index": "1",
+    "drawer_page": "",
+  };
+
+  Map<String, Map<String, String>> _pages = {
+    "shop": {
+      "appbar": "search_pp",
+      "drawer": "base_drawer",
+      "body": "shop",
+      "btmnav": "base_btmnav",
+      "btm_index": "1",
+      "drawer_page": "",
+    }
+  };
+
+  int _btm_index = 0;
+
+  void _onbtmtap(index) {
+    Map<int, String> btm_pages = {
+      0: "home",
+      1: "shop",
+      2: "cart",
+      3: "history"
+    };
+    curent_page = _pages[btm_pages[index]]!;
+    notifyListeners();
+  }
+
+  /// Data ->
+  ///
   /// <- AppBar Template
 
+  /// Appbar with search and profile picture
   AppBar search_pp(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.transparent,
@@ -30,6 +68,7 @@ class Apbr extends ChangeNotifier {
     );
   }
 
+  /// Appbar with title in center
   AppBar tit(title_) {
     return AppBar(
       centerTitle: true,
@@ -44,4 +83,39 @@ class Apbr extends ChangeNotifier {
   }
 
   /// AppBar Template ->
+
+  /// <- Body Template
+
+  ///  Body Template ->
+
+  /// <- Bottom Navigation Template
+
+  BottomNavigationBar base_btmnav() {
+    return BottomNavigationBar(
+      backgroundColor: Color(0xFF92B4EC),
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.store),
+          label: 'Shop',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_basket),
+          label: 'Cart',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.history),
+          label: 'History',
+        ),
+      ],
+      currentIndex: _btm_index,
+      onTap: _onbtmtap,
+    );
+  }
+
+  /// Bottom Navigation Template ->
 }
