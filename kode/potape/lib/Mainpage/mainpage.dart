e45437fb2157dc2280b8fdc292a9fc3636f38597
William <williamalim410@gmail.com>
2023-05-23 00:27:02 +0700

    error di carousel

--- kode/potape/lib/Mainpage/mainpage.dart
+++ kode/potape/lib/Mainpage/mainpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import '../AppBar/prov_appbar.dart';
 
 class MainPage extends StatefulWidget {
@@ -16,13 +17,7 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    ///
-    /// <- Provider
-
     final prov_apbr = Provider.of<Apbr>(context);
-
-    /// Provider ->
-    ///
 
     Map<String, AppBar> appbars = {
       "search_pp": prov_apbr.search_pp(),
@@ -43,74 +38,61 @@ class _MainPageState extends State<MainPage> {
     };
 
     return DefaultTabController(
-        length: int.parse(prov_apbr.current_page["tab_length"]!),
-        child: Scaffold(
-          appBar: appbars[prov_apbr.current_page["appbar"]],
-          drawer: drawers[prov_apbr.current_page["drawer"]],
-          body: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 20),
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                width: MediaQuery.of(context).size.width,
-                height: 225,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
+      length: int.parse(prov_apbr.current_page["tab_length"]!),
+      child: Scaffold(
+        appBar: appbars[prov_apbr.current_page["appbar"]],
+        drawer: drawers[prov_apbr.current_page["drawer"]],
+        body: Container(
+          child: Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Rekomendasi",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
+                SizedBox(
+                  height: 20,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: SizedBox(
+                    height: 200,
+                    child: Row(
                       children: [
-                        Text(
-                          'Balanced',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(right: 5),
+                          color: Colors.amber,
                         ),
-                        Text(
-                          '\$500.00',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
-                          ),
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(right: 5),
+                          color: Colors.amber,
+                        ),
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(right: 5),
+                          color: Colors.amber,
+                        ),
+                        Container(
+                          width: 200,
+                          margin: EdgeInsets.only(right: 5),
+                          color: Colors.amber,
                         ),
                       ],
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Top Up'),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                        textStyle: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        padding:
-                            EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-          bottomNavigationBar: btmnavs[prov_apbr.current_page["btmnav"]],
-        ));
+        ),
+        bottomNavigationBar: btmnavs[prov_apbr.current_page["btmnav"]],
+      ),
+    );
   }
 }
